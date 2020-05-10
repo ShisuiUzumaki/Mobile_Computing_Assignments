53270293638e9086745e805169776698bbb7a188
shisui <bsef17m051@pucit.edu.pk>
2020-05-10 22:30:22 +0500

    Add reminder layout designed somewhat

--- Term Project/Event Reminder/app/src/main/java/pk/edu/pucit/eventreminder/MainActivity.java
+++ Term Project/Event Reminder/app/src/main/java/pk/edu/pucit/eventreminder/MainActivity.java
@@ -1,9 +1,12 @@
 package pk.edu.pucit.eventreminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
           public boolean onCreateOptionsMenu(Menu menu) {
                     getMenuInflater ().inflate (R.menu.menu_main,menu);
                     return super.onCreateOptionsMenu (menu);
+          }
+
+          @Override
+          public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+                    switch(item.getItemId ()){
+                              case R.id.Add:
+                                        startActivity (new Intent (this, AddReminder.class));
+                                        break;
+                              case R.id.Settings:
+                                        break;
+                              case R.id.Help:
+                                        break;
+                              default:
+                                        // TODO  mention sth here
+                    }
+                    return true;
           }
 }
